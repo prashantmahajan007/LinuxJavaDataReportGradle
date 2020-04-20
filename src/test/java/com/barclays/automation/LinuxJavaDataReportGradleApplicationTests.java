57d6d48a69d8bdc3d8e52a30834be187fb944f51
prashantmahajan007 <63457753+prashantmahajan007@users.noreply.github.com>
2020-04-20 19:21:25 +0530

    Initial commit for Gradle project

---
+++ src/test/java/com/barclays/automation/LinuxJavaDataReportGradleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.barclays.automation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinuxJavaDataReportGradleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
